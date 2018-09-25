601f794e944fb88a41d5b84c8d3bf7c5f1fae21c
fatboy <365078225@qq.com>
2018-09-25 16:29:09 +0800

    +subplot demo

--- subplot_demo.py
+++ subplot_demo.py
@@ -9,7 +9,9 @@ fig1 = plt.figure(figsize=(10, 5))
 # 121 means: 1 row, 2 col, first zone
 sub1 = fig1.add_subplot(121)
 sub1.plot(x, x)
+sub1.set_title("121")
 sub2 = fig1.add_subplot(122)
 sub2.plot(x, -x)
+sub2.set_title("122")
 
 plt.show()
